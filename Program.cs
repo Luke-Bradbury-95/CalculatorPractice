092dc21689d1d0ea578d97186f7eeecd972241e8
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-02-29 20:08:20 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Please enter your first number");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter your second number");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the symbol for the math you wish to do");
+char math = char.Parse(Console.ReadLine());
+
+switch (math)
+{
+    case '+':
+        Console.WriteLine($"{num1} + {num2} =  {num1 + num2}");
+        break;
+    case '-':
+        Console.WriteLine($" {num1} - {num2} = {num1 - num2}");
+        break;
+    case '*':
+        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        break;
+    case '/':
+        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+        break;
+}
+
+Console.ReadLine();
